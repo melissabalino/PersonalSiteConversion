c9821b95e90875d857b0c7aacdb3c7aaadec4330
Melissa Balino <mbalino.devsolutions@outlook.com>
2023-08-18 14:01:07 -0500

    Add project files.

---
+++ PersonalSiteConversion/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalSiteConversion.Models;
+using System.Diagnostics;
+using MimeKit; //Added for access to MimeMessage class
+using MailKit.Net.Smtp; //Added for access to SmtpClient class
+
+namespace PersonalSiteConversion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have received a new email from your site's contact form!<br />" +
+               $"Sender: {cvm.Name}<br />Email: {cvm.Email}<br />Subject: {cvm.Subject}<br />" +
+               $"Message: {cvm.Message}";
+
+            var mm = new MimeMessage();
+
+            mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+
+            mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+            mm.Subject = cvm.Subject;
+
+            mm.Body = new TextPart("HTML") { Text = message };
+
+            mm.Priority = MessagePriority.Urgent;
+
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_config.GetValue<string>("Credentials:Email:Client"), 8889);
+                client.Authenticate(
+                    _config.GetValue<string>("Credentials:Email:User"),
+                    _config.GetValue<string>("Credentials:Email:Password")
+                );
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Error= $"There wan error processing your request. Please try again later." +
+                        $"<br />Error Message: {ex.StackTrace}";
+                    return RedirectToAction("Index", new {ID = "contact"});
+                }
+            }
+            return View("EmailConfirmation", cvm);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PersonalSiteConversion/Models/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteConversion.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "*Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is required")]
+        [DataType(DataType.EmailAddress)] 
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
